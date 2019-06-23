3d32f364046621cb4bcd3f9db7a0e0cdd9d1062e
vagusX <vagusxl@gmail.com>
2019-06-23 18:32:55 +0800

    refactor: implements with extending passport.Authenticator

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-import 'egg';
+import { Request } from 'egg';
 
-import * as passport from 'passport';
+import { AuthenticateOptions, Strategy, Authenticator } from 'passport';
 
 interface IMountOptions {
   loginURL: string;
@@ -8,22 +8,33 @@ interface IMountOptions {
 }
 
 declare module 'egg' {
+  interface EggPassport extends Authenticator {
+    authenticate<AuthenticateRet = any>(strategy: string | string[]): AuthenticateRet;
+    authenticate<AuthenticateRet = any>(strategy: string | string[], options: AuthenticateOptions): AuthenticateRet;
+
+    session<AuthenticateRet = any>(): AuthenticateRet;
+
+    mount(strategy: string, options?: IMountOptions): void;
+
+    doVerify<TUserPayload = any, TUser = any>(req: Request, user: TUserPayload, done: (err: any, user?: TUser) => void): void;
+
+    verify<TUser = any, TID = any>(fn: (ctx: Context, user: TID) => Promise<TUser>): void;
+
+    serializeUser<TUser = any, TID = any>(fn: (ctx: Context, user: TUser) => TID): void;
+    serializeUser<TID = any>(user: TID, req: Request, done: (err: any, user?: TID) => void): void;
+
+    deserializeUser<TUser = any, TID = any>(fn: (ctx: Context, id: TID) => void): void;
+    deserializeUser<TUser = any, TID = any>(fn: (ctx: Context, id: TID) => Promise<TUser>): void;
+    deserializeUser<TUser = any, TID = any>(ctx: Request, id: TID): void;
+    deserializeUser<TUser = any, TID = any>(ctx: Request, id: TID): Promise<TUser>;
+
+    _handleSerializeUser<TUser = any, TID = any>(ctx: Context, user: TUser, done: (err: any, user?: TID) => void): void;
+    _handleDeserializeUser<TUser = any, TID = any>(ctx: Context, id: TID): TID | TUser;
+  }
+
   // extend app
   interface Application {
-    passport: {
-      serializeUser<TUser = any, TID = any>(fn: (ctx: Context, user: TUser) => Promise<TID>)
-      serializeUser<TUser = any, TID = any>(fn: (user: TUser, done: (err: any, id?: TID) => void) => void): void;
-
-      deserializeUser<TUser = any, TID = any>(fn: (ctx: Context, id: TID) => Promise<TUser>)
-      deserializeUser<TUser = any, TID = any>(fn: (id: TID, done: (err: any, user?: TUser) => void) => void): void;
-
-      verify(fn: (ctx: Context, user: any) => Promise<any>);
-
-      mount(strategy: string, options?: IMountOptions): void;
-
-      authenticate(strategy: string | string[]): any;
-      authenticate(strategy: string | string[], options: passport.AuthenticateOptions): any;
-    }
+    passport: EggPassport;
   }
 
   // extend context
@@ -40,7 +51,7 @@ declare module 'egg' {
     secret: string;
   }
 
-  // extend your config
+  // extend config
   interface EggAppConfig {
     passportLocal?: {
       usernameField?: string;
